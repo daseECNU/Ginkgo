f98d7f6f18e6122c64051525fc86d7eb570e9c49
imdb <285649461@qq.com>
2017-03-24 11:58:56 +0800

    Add the function of loading from hdfs to hdfs.

---
+++ loader/hdfs_loader.cpp
@@ -0,0 +1,97 @@
+/*
+ * Copyright [2012-2017] DaSE@ECNU
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * /CLAIMS/stmt_handler/load_exec.cpp
+ *
+ *  Created on: Feb 22, 2017
+ *      Author: hurry.huang
+ *       Email: hurry.huang@infosys.com
+ *
+ * Description:
+ *    this file is the function body of class LoadExec.
+ */
+
+//#include <string>
+#include "hdfs_loader.h"
+#include "../common/error_define.h"
+#include "../common/file_handle/hdfs_connector.h"
+#include <string>
+using namespace std;
+using namespace claims::common;
+
+namespace claims{
+namespace loader{
+
+	HdfsLoader::HdfsLoader(){
+
+	}
+	HdfsLoader::~HdfsLoader(){
+		hdfsDisconnect(fs_);
+	}
+	RetCode HdfsLoader::CheckHdfsFile(string file_name){
+		int ret = rSuccess;
+		hdfsFileInfo* hdfsfile = hdfsGetPathInfo(fs_,file_name.c_str());
+		if(NULL == hdfsfile){
+			ret = rFailure;
+		}
+		return ret;
+
+	}
+
+	RetCode HdfsLoader::PrepareForLoadFromHdfs(){
+		int ret = rSuccess;
+		fs_ = claims::common::HdfsConnector::Instance();
+		if (NULL == fs_)
+			ret = rFailure;
+		return ret;
+	}
+
+	int HdfsLoader::GetCharFromBuffer(void*& buffer, int & pos, int & read_num, const int & length,
+													int & total_read_num){
+		if(pos == read_num){
+			read_num = hdfsRead(fs_,file_,static_cast<char*>(buffer),length);
+			pos = 0;
+			if(read_num == 0){
+				pos = 0;
+
+				return -1;
+
+			}
+		}
+		total_read_num++;
+		return *((char*)buffer + pos++);
+		//return ' ';
+
+	}
+
+	RetCode HdfsLoader::OpenHdfsFile(string & file_name){
+		int ret = rSuccess;
+		file_ = hdfsOpenFile(fs_, file_name.c_str(),O_RDONLY,0,0,0);
+		if (!file_) {
+			ret = rFailure;
+	  }
+		return ret;
+
+
+
+	}
+
+
+
+}
+}
---
+++ loader/hdfs_loader.h
@@ -0,0 +1,68 @@
+/*
+ * Copyright [2012-2015] DaSE@ECNU
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * /CLAIMS/stmt_handler/load_exec.h
+ *
+ *  Created on: feb 22, 2017
+ *      Author: hurry_huang
+ *	     Email: hurry.huang@infosys.com
+ *
+ * Description:
+ *      this file contains one class about load data from hdfs.
+ *
+ */
+
+#ifndef STMT_HANDLER_HDFS_LOAD_EXEC_H
+#define STMT_HANDLER_HDFS_LOAD_EXEC_H
+
+//#include "../stmt_handler/stmt_exec.h"
+#include <hdfs.h>
+#include <string>
+#include "../common/error_define.h"
+using std::string;
+
+namespace claims{
+namespace loader{
+/**
+ * @brief
+ * @details
+ */
+class HdfsLoader{
+public:
+	HdfsLoader();
+	virtual ~HdfsLoader();
+	//RetCode Execute (ExecutedResult* exec_result);
+	 RetCode CheckHdfsFile(string file_name);
+	 RetCode PrepareForLoadFromHdfs();
+	 int GetCharFromBuffer(void*& buffer, int & pos, int & read_num, const int & length,
+			 	 	 	 	 	 	 	  int & total_read_num);
+	 RetCode GetFromHdfs(const string& file_name,int & length);
+	 RetCode OpenHdfsFile(string & file_name);
+private:
+	//AstLoadTable* load_ast_;
+	hdfsFS fs_;
+	hdfsFile file_;
+	//common::FileHandleImp* imp_;
+
+};
+
+
+}
+}
+
+#endif
